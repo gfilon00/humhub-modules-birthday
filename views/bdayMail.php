288e7c4ce4b52fda31318262973ba044e374ae17
gfilon <gfilon@enclave.com.ar>
2016-10-03 10:39:23 -0300

    Mejora en el estilo del mail de los cumpleaños

--- views/bdayMail.php
+++ views/bdayMail.php
@@ -2,20 +2,26 @@
 	<div style="clear: both;">
 		<h2><?php echo Yii::t('BirthdayModule.base', 'Birthdays'); ?></h2>
 	</div>
-	<div style="clear: both;">
-		<h2><?php echo Yii::t('BirthdayModule.base', 'today'); ?></h2>
-	</div>
-	<table>
-		<?php foreach ($todayers as $user) {
-			echo '</tr>' . $user->profile->lastname . ', ' . $user->profile->firstname . '</tr><br/>';
-		} ?>
-	</table>
-	<div style="clear: both;">
-		<h2><?php echo Yii::t('BirthdayModule.base', 'Tomorrow'); ?></h2>
-	</div>
-	<table>
-		<?php foreach ($tomorrowers as $user) {
-			echo '</tr>' . $user->profile->lastname . ', ' . $user->profile->firstname . '</tr><br/>';
-		} ?>
-	</table>
+	<?php if (count($todayers)){ ?>
+		<div style="clear: both;">
+			<h2><?php echo Yii::t('BirthdayModule.base', 'today'); ?></h2>
+		</div>
+		<table>
+			<?php foreach ($todayers as $user) {
+				echo '</tr>' . $user->profile->lastname . ', ' . $user->profile->firstname . '</tr><br/>';
+			} ?>
+		</table>
+	<?php } ?>
+
+	<?php if (count($tomorrowers)) { ?>	
+		
+		<div style="clear: both;">
+			<h2><?php echo Yii::t('BirthdayModule.base', 'Tomorrow'); ?></h2>
+		</div>
+		<table>
+			<?php foreach ($tomorrowers as $user) {
+				echo '</tr>' . $user->profile->lastname . ', ' . $user->profile->firstname . '</tr><br/>';
+			} ?>
+		</table>
+	<?php } ?>
 </div>
